964751ecd172b68ee822448589d54b9d78a5dcb0
rohitsharma <rs0043071@gmail.com>
2025-06-06 10:12:33 +0530

    Resolve merge conflicts

--- frontend/src/components/chatContainer.jsx
+++ frontend/src/components/chatContainer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 
-import ChatHeader from "./ChatHeader";
-import MessageInput from "./MessageInput";
+import ChatHeader from "./chatHeader";
+import MessageInput from "./messageInput";
 import MessageSkeleton from './messageSkeleton';
 import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
